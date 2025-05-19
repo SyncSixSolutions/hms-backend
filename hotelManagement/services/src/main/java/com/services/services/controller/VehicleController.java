fe3ac572ca258f6cbae86ef84e5edc82a27f7454
nadun_hasalanka <nadun202327@gmail.com>
2025-05-19 22:51:38 +0530

    [HMS-5] changed the things for controller of the vehicle and also with the api gateway to

---
+++ hotelManagement/services/src/main/java/com/services/services/controller/VehicleController.java
@@ -0,0 +1,14 @@
+package com.services.services.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static jdk.internal.foreign.abi.aarch64.AArch64Architecture.Regs.v1;
+
+@RestController
+@CrossOrigin
+@RequestMapping("api/v1/vehicle")
+public class VehicleController {
+
+}
